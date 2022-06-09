6d7446752e781a21daa5fb96e63934ec13700454
Vidhixa Joshi <vidhixa@squareup.com>
2022-06-09 15:07:10 -0500

    Rename cocktail to cocktails

--- api/CocktailsMenuHandler.py
+++ api/CocktailsMenuHandler.py
@@ -1,7 +1,7 @@
 from flask_restful import Api, Resource, reqparse
 import json
 
-class CocktailMenuHandler(Resource):
+class CocktailsMenuHandler(Resource):
     def get(self):
-        f = open('resources/json/CocktailMenu.json')
+        f = open('resources/json/CocktailsMenu.json')
         return json.load(f)
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
-from api.CocktailMenuHandler import CocktailMenuHandler
+from api.CocktailsMenuHandler import CocktailsMenuHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
@@ -13,6 +13,6 @@ def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
-api.add_resource(CocktailMenuHandler, '/cocktails')
+api.add_resource(CocktailsMenuHandler, '/cocktails')
 
 
