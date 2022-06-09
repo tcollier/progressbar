d004e48510b817edc0ffad8f2a816b4a64868f14
Tom Collier <tcollier@squareup.com>
2022-06-09 17:04:37 -0500

    Allow accordian to fully close after opening

--- frontend/src/CocktailList.js
+++ frontend/src/CocktailList.js
@@ -24,7 +24,7 @@ function CocktailBody({ cocktail }) {
 }
 function CocktailList({ cocktails }) {
   return (
-      <Accordion>
+      <Accordion allowToggle>
         {cocktails.map(function(cocktail) {
           return (
               <AccordionItem>
