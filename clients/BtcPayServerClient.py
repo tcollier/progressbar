12d221930ab0cd0f12886c66c226aedcbc8f0b9a
stephaniehsu-square <stephaniehsu@squareup.com>
2022-06-10 10:29:53 -0500

    Add order id to invoice and reset server url to env variable

--- clients/BtcPayServerClient.py
+++ clients/BtcPayServerClient.py
@@ -6,13 +6,14 @@ class BtcPayServerClient:
     def __init__(self):
         f = open("btcpayclient.creds", "rb")
         self.client = pickle.load(f)
-        self.server_url = "https://btcpp-progress-bar.herokuapp.com"
+        self.server_url = os.environ['SERVER_URL']
 
     def create_invoice(self, item_id, item_price):
         invoice = self.client.create_invoice({"price": item_price, "currency": "SATS",
                                     "notificationURL": self.server_url + "/btcPay/invoiceSettled?itemId=" + item_id,
                                     "redirectURL": self.server_url + "/btcPay/invoiceCreated",
                                     "closeURL": self.server_url + "/btcPay/invoiceClosed",
+                                    "orderId": item_id,
                                     "itemId": item_id, "itemDesc": "This is our item description",
                                     "physical": True})
         return invoice["url"]
