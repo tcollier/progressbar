3b3d296274333039eabfc753d601a3a09f8464ba
Tom Collier <tcollier@squareup.com>
2022-06-09 19:51:46 -0500

    Add darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import CocktailList from './CocktailList'
 import { useCocktailMenuFetcher } from "./hooks";
+import {Button, Spinner, useColorMode} from "@chakra-ui/react";
+import { MoonIcon } from "@chakra-ui/icons";
 
 function App() {
-  const { cocktails, loading, error } = useCocktailMenuFetcher()
+  const { cocktails, loading } = useCocktailMenuFetcher()
+  const { toggleColorMode } = useColorMode()
 
   return (
       <div className="App">
+        <header>
+          <Button onClick={toggleColorMode}>
+            <MoonIcon />
+          </Button>
+        </header>
+        { loading &&  <Spinner /> }
         {cocktails && <CocktailList cocktails={cocktails} />}
       </div>
   );
